f2ec25dd886dab8c474dd27991b4c72afcf2f822
Sergio Castillo Checa <Sergio Castillo Checa>
2016-10-06 16:56:38 +0200

    La modal se abre desde un botón

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,8 @@ let bs = bootStrap;
 
 $(() => {
     var g = new Greeter();
+    $('#launchBtn').click(function() {
+        $('#myModal').modal();
+    });
     $('#title-text').html(g.greet('Kash'));
-    $('#myModal').modal();
 });
