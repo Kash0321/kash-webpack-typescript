9a29a9bbc3e1ffefd931de9acbf957d860e0074e
Sergio Castillo <sergio.castillo@server.pronet-ise.com>
2016-10-07 11:06:23 +0200

    Eliminación de ficheros resultado del código fuente

--- src/dist/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var greeter_1 = require('./greeter');
-var $ = require('jquery');
-$(function () {
-    var g = new greeter_1.Greeter();
-    $('#title-text').html(g.greet('Kash'));
-});
-//# sourceMappingURL=app.js.map
