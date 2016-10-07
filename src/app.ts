259514b2d930f0d91c91d1d2ab857e9214232f17
Sergio Castillo <sergio.castillo@server.pronet-ise.com>
2016-10-07 11:03:17 +0200

    CSS integrado con webpack en un solo bundle js. Minificación y sourcemaps

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,16 @@
+declare var require: {
+    <T>(path: string): T;
+    (paths: string[], callback: (...modules: any[]) => void): void;
+    ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
+};
+
 import { Greeter } from './greeter'
 import * as $ from 'jquery';
+
+require('./node_modules/bootstrap/dist/css/bootstrap.css');
+require('./node_modules/bootstrap/dist/css/bootstrap-theme.css');
+require('./css/style.css');
+
 import bootStrap = require('bootstrap');
 let bs = bootStrap;
 
--- src/webpack.config.js
+++ src/webpack.config.js
@@ -7,20 +7,26 @@ module.exports = {
         path: __dirname + '/dist',
         filename: "bundle.js"
     },
+    // Turn on sourcemaps
+    devtool: 'source-map',
     resolve: {
-        // Add '.ts' and '.tsx' as a resolvable extension.
-        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+        // Resolvable extensions.
+        extensions: ["", ".webpack.js", ".web.js", ".css", ".ts", ".tsx", ".js"]
     },
     plugins: [
         new webpack.ProvidePlugin({
            $: "jquery",
            jQuery: "jquery"
-       })
+        }),
+        new webpack.optimize.UglifyJsPlugin()
     ],
     module: {
         loaders: [
             // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
-            { test: /\.tsx?$/, loader: "ts-loader" }
+            { test: /\.tsx?$/, loader: "ts-loader" },
+            { test: /\.css$/, loader: "style-loader!css-loader" },
+            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
+            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
         ]
     }
 };
