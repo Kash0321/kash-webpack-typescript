7b2db9630fccb4239235e18447555b88d9c20316
Sergio Castillo <sergio.castillo@server.pronet-ise.com>
2016-10-14 09:50:16 +0200

    Ajustes bootstrap

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 module.exports = {
     entry: "./app.ts",
     output: {
-        path: __dirname,
+        path: __dirname + '/dist',
         filename: "bundle.js"
     },
     // Turn on sourcemaps
@@ -24,9 +24,20 @@ module.exports = {
         loaders: [
             // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
             { test: /\.tsx?$/, loader: "ts-loader" },
+
             { test: /\.css$/, loader: "style-loader!css-loader" },
-            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
-            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
+
+            // **IMPORTANT** This is needed so that each bootstrap js file required by
+            // bootstrap-webpack has access to the jQuery object
+            { test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery' },
+
+            // Needed for the css-loader when [bootstrap-webpack](https://github.com/bline/bootstrap-webpack)
+            // loads bootstrap's css.
+            { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&minetype=application/font-woff" },
+            { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,  loader: "url?limit=10000&minetype=application/font-woff2" },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&minetype=application/octet-stream" },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&minetype=image/svg+xml" }
         ]
     }
 };
